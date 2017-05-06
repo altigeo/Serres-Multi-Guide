9bcb45d68e72b66a85af8381e4d24d57efcf10cd
Altintzis Georgios <e.agores.ag@gmail.com>
2017-05-06 17:37:20 +0300

    -

--- connect.php
+++ connect.php
@@ -10,15 +10,4 @@
 	if (mysqli_connect_errno($con)) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-	$result = mysqli_query($con,"SELECT * FROM sites where Username='$username' and Password='$password'");
-	$row = mysqli_fetch_array($result);
-	$data = $row[0];
-
-	if($data){
-		echo $data;
-	}
-	mysqli_close($con);
 ?>
